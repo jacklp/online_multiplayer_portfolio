d42e99e26cacff17e27d1a8189ec6ba5edbd0c07
jacklp <jackleonprior@gmail.com>
2016-02-27 02:55:17 +0000

    Online Multiplayer working + Player Animation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Photon.MonoBehaviour {
+
+	public string playerPrefabName = "Charprefab";
+
+	void OnJoinedRoom()
+	{
+		StartGame();
+	}
+	
+	void StartGame()
+	{
+		Camera.main.farClipPlane = 1000; //Main menu set this to 0.4 for a nicer BG
+
+		// Spawn our local player
+		PhotonNetwork.Instantiate(this.playerPrefabName, transform.position, Quaternion.identity, 0);
+	}
+
+	void OnGUI()
+	{
+		if (PhotonNetwork.room == null) return; //Only display this GUI when inside a room
+
+		if (GUILayout.Button("Leave Room"))
+		{
+			PhotonNetwork.LeaveRoom();
+		}
+	}
+
+	void OnDisconnectedFromPhoton()
+	{
+		Debug.LogWarning("OnDisconnectedFromPhoton");
+	}   
+}
---
+++ Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMobility : MonoBehaviour{
+
+	public float speed;
+	public Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
+
+	void FixedUpdate()
+	{
+
+		if(Input.GetKey(KeyCode.A)){
+			anim.SetBool("Left", true);
+			anim.SetBool("Up", false);
+			anim.SetBool("Right", false);
+			anim.SetBool("Down", false);
+		}
+
+		if(Input.GetKey(KeyCode.W)){
+			anim.SetBool("Up", true);
+			anim.SetBool("Down", false);
+			anim.SetBool("Left", false);
+			anim.SetBool("Right", false);
+		}
+
+		if(Input.GetKey(KeyCode.D)){
+			anim.SetBool("Right", true);
+			anim.SetBool("Left", false);
+			anim.SetBool("Up", false);
+			anim.SetBool("Down", false);
+		}
+
+		if(Input.GetKey(KeyCode.S)){
+			anim.SetBool("Down", true);
+			anim.SetBool("Left", false);
+			anim.SetBool("Right", false);
+			anim.SetBool("Up", false);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			anim.SetBool ("WalkLeft", true);
+		} else {
+			anim.SetBool ("WalkLeft", false);
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			anim.SetBool ("WalkUp", true);
+		} else {
+			anim.SetBool ("WalkUp", false);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			anim.SetBool ("WalkRight", true);
+		} else {
+			anim.SetBool ("WalkRight", false);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			anim.SetBool ("WalkDown", true);
+		} else {
+			anim.SetBool ("WalkDown", false);
+		}
+
+	}
+}
+
+
